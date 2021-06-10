65a7376f953700f3c5be9a49382015bd41a35860
ngndevil <78074799+ngndevil@users.noreply.github.com>
2021-06-10 20:20:29 +0700

    check

--- shop.js
+++ shop.js
@@ -1,4 +1,19 @@
-var pass = document.getElementById("password");
-var email = document.getElementById("email");
-console.log(pass)
-console.log(email)
+const togglePassword = document.querySelector('#togglePassword');// hide/show passwwork
+const password = document.querySelector('#password');
+togglePassword.addEventListener('click', function (e){
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+}); 
+
+function checkpass(){
+    var pass = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    if(pass != "admin" || email != "admin"){
+       document.getElementById("wrong").innerHTML = "Sai username hoặc passwword! Mời nhập lại";
+    }
+    else
+        document.getElementById("wrong").innerHTML = "";
+}
+
+document.getElementById("login").onclick = function() {checkpass()};

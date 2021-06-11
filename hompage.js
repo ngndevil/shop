ffe191b593532fb8c650d57b4ef5860b145d5f84
ngndevil <78074799+ngndevil@users.noreply.github.com>
2021-06-11 22:50:06 +0700

    show hide list

---
+++ hompage.js
@@ -0,0 +1,21 @@
+var stt = false;
+var id = ["rf","pt","mg","smg","sr","rk","gn","am","asr"];
+function showhidelist(){
+    if(stt == false){
+        for(let i = 0;i < id.length;i++){
+            var nameid = id[i]
+            document.getElementById(nameid).style.display = "block";
+        }
+        stt = true;
+    }else{
+        for(let i = 0;i < id.length;i++){
+            var nameid = id[i]
+            document.getElementById(nameid).style.display = "none";
+        }
+       stt = false;
+    }
+}
+function check(){
+    console.log(1)
+}
+document.getElementById("dssp").onclick = function() {showhidelist()};
